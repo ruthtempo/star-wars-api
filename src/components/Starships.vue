7214387f1d25f2503105f2836eb8ae66e6eb6bbb
Ruth <ruth@Deniss-Air.home>
2021-11-28 18:24:34 +0100

    exercici 3

--- src/components/Starships.vue
+++ src/components/Starships.vue
@@ -1,20 +1,24 @@
 <template>
+  <div>
     <ul class="starshipList">
-        <li class="starship" v-for="(starship, index) in $store.state.starships" :key="index">
-          <router-link :to="{path:'starshipfile', query: {index}}">
-            Starship Name: {{starship.name}} <br>
-            Model:{{starship.model}}
-          </router-link> 
-        </li>
+      <li class="starship" v-for="(starship, index) in $store.state.starships" :key="index">
+        <router-link :to="{path:'starshipfile', query: {index}}">
+          Starship Name: {{starship.name}} <br>
+          Model:{{starship.model}}
+        </router-link> 
+      </li>
     </ul>
+    <button v-if="$store.state.nextPageUrl" :disabled="$store.state.loading" @click="$store.dispatch('fetchStarships')">View More</button>
+  </div>
 </template>
 
 <script>
 export default {
     name:"Starships",
     mounted(){
+        this.$store.commit('resetStarshipList')
         this.$store.dispatch('fetchStarships')
-    }
+    },
 }
 </script>
 <style scoped>
@@ -29,6 +33,11 @@ export default {
     cursor:pointer;
     list-style-type: none;
    
+}
+
+button{
+  padding:5px;
+  cursor:pointer;
 }
 
 </style>
--- src/store.js
+++ src/store.js
@@ -4,29 +4,42 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-
     state:{
-        starships: []
+        starships: [],
+        nextPageUrl:"https://swapi.dev/api/starships",
+        loading: false //to prevent from doubleclickling -button wi be disabled when true
     },
     mutations:{
-      saveStarships(state, starships){
-        state.starships = starships
+      saveStarships(state, starshipsNextPage){
+        const newStarshipsList = state.starships.concat(starshipsNextPage)
+        state.starships = newStarshipsList
 
-        starships.forEach(starship => {
+        starshipsNextPage.forEach(starship => {
           const id = parseInt(starship.url.slice(32,-1))
-          starship.id= id
+          starship.id = id
           
         });
-        
+      },
+      updatePageUrl(state, link){
+        state.nextPageUrl = link
+      },
+      resetStarshipList(state){
+        state.starships = []
+        state.nextPageUrl= "https://swapi.dev/api/starships"
+        console.log(state.starships)
       }
     },
     actions:{
       fetchStarships(context){
-        fetch("https://swapi.dev/api/starships")
+        this.state.loading = true
+        fetch(context.state.nextPageUrl)
         .then(response=>response.json())
-        .then(response =>{
-            context.commit("saveStarships", response.results)
+        .then(response=>{
+            this.state.loading =false
+            context.commit('saveStarships', response.results)
+            context.commit('updatePageUrl', response.next)
         })
+
       }
 
     },
